76f1ab4f908a485eb815afde8d11d6968fdcd7c1
seijihosokawa <seijihosokawa@gmail.com>
2021-06-11 16:45:41 -0400

    list years and limit added

--- server/controllers/years.js
+++ server/controllers/years.js
@@ -2,9 +2,19 @@ import Years from '../models/yearsList.js';
 
 export const getYears = async (req, res) => {
     try {
-        const year = await Years.find();
+        const year = await Years.find().limit(200);
 
         res.status(200).json(year);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getYearsList = async (req, res) => {
+    try {
+        const yearList = await Years.find().distinct('year');
+
+        res.status(200).json(yearList);
     } catch (error) {
         res.status(404).json({message: error.message});
     }
--- server/routes/years.js
+++ server/routes/years.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import {getYears} from '../controllers/years.js';
+import {getYears, getYearsList} from '../controllers/years.js';
 
 const router = express.Router();
 
 router.get('/', getYears);
+router.get('/list', getYearsList);
 
 export default router;
