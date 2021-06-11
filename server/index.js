4c19f4993468aa681f1064100e04c0826498923a
seijihosokawa <seijihosokawa@gmail.com>
2021-06-10 22:49:03 -0400

    added router and dotenv package

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(express.json())
+app.use('/posts', postRoutes);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://seijihdev:<password>@cluster0.sac6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+dotenv.config();
+
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.sac6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+const PORT = process.env.PORT;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error));
+mongoose.set('useFindAndModify', false);
