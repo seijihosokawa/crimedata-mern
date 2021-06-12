494485134b7a3ea18f589cb6c5857808e1da2f79
seijihosokawa <seijihosokawa@gmail.com>
2021-06-12 10:39:51 -0400

    updated api endpoints

--- server/controllers/crimes.js
+++ server/controllers/crimes.js
@@ -2,7 +2,9 @@ import Years from '../models/crimeSchema.js';
 
 export const getData = async (req, res) => {
     try {
-        const year = await Years.find().limit(200);
+        var limit  = req.query.limit ? parseInt(req.query.limit) : 200;
+        
+        const year = await Years.find().limit(limit);
 
         res.status(200).json(year);
     } catch (error) {
@@ -12,7 +14,9 @@ export const getData = async (req, res) => {
 
 export const getYears = async (req, res) => {
     try {
-        const yearList = await Years.find().distinct('year');
+        var limit  = req.query.limit ? parseInt(req.query.limit) : 200;
+
+        const yearList = await Years.find().distinct('year').limit(limit);
 
         res.status(200).json(yearList);
     } catch (error) {
@@ -22,7 +26,9 @@ export const getYears = async (req, res) => {
 
 export const getStates = async (req, res) => {
     try {
-        const state = await Years.find().distinct('state_abbr');
+        var limit  = req.query.limit ? parseInt(req.query.limit) : 200;
+
+        const state = await Years.find().distinct('state_abbr').limit(limit);
 
         res.status(200).json(state);
     } catch (error) {
@@ -32,9 +38,36 @@ export const getStates = async (req, res) => {
 
 export const getStatesList = async (req, res) => {
     try {
-        const stateList = await Years.find().distinct('state_name');
+        var limit  = req.query.limit ? parseInt(req.query.limit) : 200;
+
+        const stateList = await Years.find().distinct('state_name').limit(limit);
 
         res.status(200).json(stateList);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getSpecificYear = async (req, res) => {
+    try {
+        var year = req.params.year; 
+
+        const specificYear = await Years.find({'year':year}).limit(limit);
+
+        res.status(200).json(specificYear);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getSpecificState = async (req, res) => {
+    try {
+        var limit  = req.query.limit ? parseInt(req.query.limit) : 200;
+        var state = req.params.id.toUpperCase(); 
+
+        const specificState = await Years.find({'state_abbr':state}).limit(limit);
+
+        res.status(200).json(specificState);
     } catch (error) {
         res.status(404).json({message: error.message});
     }
--- server/routes/crimes.js
+++ server/routes/crimes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import {getData, getYears, getStates, getStatesList} from '../controllers/crimes.js';
+import {getData, getYears, getStates, getStatesList, getSpecificYear, getSpecificState} from '../controllers/crimes.js';
 
 const router = express.Router();
 
 router.get('/', getData);
 router.get('/years', getYears);
+router.get('/years/:year', getSpecificYear);
 router.get('/states', getStates);
+router.get('/states/:id', getSpecificState);
 router.get('/states-list', getStatesList);
 
 export default router;
