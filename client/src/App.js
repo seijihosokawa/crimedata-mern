79cf4b10daa5efe196e29c74b5aac221530799dd
seijihosokawa <seijihosokawa@gmail.com>
2021-06-16 16:06:34 -0400

    testing deploy

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ const App = () => {
                 <SideBar></SideBar>
             </div>
             <div id="content" className="container mx-auto py-10 h-full md:w-4/5 px-6 divide-y divide-gray-400 pl-32">
-                {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
                 <div className="flex flex-row w-full pb-12">
                     <div id="intro" className="container w-1/2 p-4">
                         <h1 className="text-gray-100 text-3xl">Introduction</h1>
