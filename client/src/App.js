a8c9b6b55af1d10e21dc3130704728c8e416b3d8
seijihosokawa <seijihosokawa@gmail.com>
2021-06-16 00:37:44 -0400

    updated client api docs

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+// eslint-disable-next-line
 import SwaggerUi from 'swagger-ui-react';
+// eslint-disable-next-line
 import "swagger-ui-react/swagger-ui.css"
+import SideBar from './sidebar';
 
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-            <SwaggerUi url="https://app.swaggerhub.com/apis-docs/seijih/crimes/1.0.0/swagger.json"/>
-            <button href="https://app.swaggerhub.com/apis-docs/seijih/crimes/1.0.0">Click here for documentation</button>
-            
+        <div className="flex flex-no-wrap h-screen bg-gradient-to-r from-gray-800 to-blue-800">
+            <div className="app__sidebar">
+                <SideBar></SideBar>
+            </div>
+            {/*
+            <SwaggerUi url="https://raw.githubusercontent.com/seijihosokawa/seijihosokawa.github.io/master/assets/swagger_crimes.json"/>
+            */}
+            <div className="container mx-auto py-10 h-full md:w-4/5 w-11/12 px-6">
+                {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
+                <div className="w-full h-full rounded border-dashed border-2 border-gray-300">{/* Place your content here */}</div>
+            </div>
         </div>
     )
 }
---
+++ client/src/sidebar.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+
+function SideBar() {
+    const [hideSummary, sethideSummary] = useState(true);
+    return (
+            <div className="w-48 sm:relative h-full bg-gray-900 shadow flex-col justify-between sm:flex">
+                <div className="px-8">
+                    <div className="w-full h-16 flex items-center text-white">
+                        {/* heading starts */}
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                        </svg>
+                        Crimes API  
+
+                    </div>
+                    <ul className="mt-2">
+                        <li className="flex w-full justify-between text-gray-300 hover:text-blue-300 cursor-pointer items-center mb-4">
+                            <div className="flex items-center">
+                                <span className="text-sm  ml-2">Introduction</span>
+                            </div>
+                        </li>
+                        <li className="flex w-full justify-between text-gray-300 hover:text-blue-300 cursor-pointer items-center mb-4">
+                            <div className="flex items-center">
+                                <span className="text-sm  ml-2">References</span>
+                            </div>
+                        </li>
+                        <li className="flex w-full justify-between text-gray-300 hover:text-blue-300 cursor-pointer items-center mb-4" onClick={() => sethideSummary(!hideSummary)}>
+                            <div className="flex items-center">
+                                <span className="text-sm  ml-2">Summary</span>
+                                { hideSummary === true &&
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-14 mt-1" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                                }
+                                { hideSummary === false &&
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-14 mt-1" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                                </svg>
+                                }
+                            </div>
+                        </li>
+                        { hideSummary === false &&
+                        <div>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Full Data</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get List of Years</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get Specific Years</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get Range of Years</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get Year Totals</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get Range of Year Totals</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get List of States Abbreviated</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get a Specific State</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get List of States Full</span>
+                                </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-500 hover:text-gray-200 cursor-pointer items-center mb-4">
+                                <div className="flex items-center">
+                                    <span className="text-sm  ml-3">Get List of Crimes Tracked</span>
+                                </div>
+                            </li>
+                        </div>
+                        }
+                        <li className="flex w-full justify-between text-gray-300 hover:text-blue-300 cursor-pointer items-center mb-4">
+                            <div className="flex items-center">
+                                <span className="text-sm  ml-2">Next Data</span>
+                            </div>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </div>
+    );
+}
+
+export default SideBar;
