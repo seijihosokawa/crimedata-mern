61dae0207c49127af5c959999d5da56d488078f8
seijihosokawa <seijihosokawa@gmail.com>
2021-06-17 11:26:18 -0400

    added port

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.sac6s.mongodb.net/crime_data?retryWrites=true&w=majority`
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
