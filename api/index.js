20062c765f634e880cf300f8711be0f3dafd3004
seijihosokawa <seijihosokawa@gmail.com>
2021-06-17 09:28:57 -0400

    updates

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/summary', summaryRoutes);
+app.use('/v1/summary', summaryRoutes);
 
 
 dotenv.config();
