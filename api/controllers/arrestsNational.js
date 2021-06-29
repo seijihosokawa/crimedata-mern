71d56d8475f959688a0b87b6fec7e32644a601af
seijihosokawa <seijihosokawa@gmail.com>
2021-06-29 18:38:49 -0400

    arrest years routes

--- api/controllers/arrestsNational.js
+++ api/controllers/arrestsNational.js
@@ -3,13 +3,13 @@ import Arrests from '../models/arrestsSchema.js';
 export const getData = async (req, res) => {
     try {
         var limit = parseInt(req.query.limit);
-        var crime = req.query.crime;
+        var queryString = req.query.fields;
 
-        if(crime !== undefined){
-            let queryParams = crime.split(',');
+        if(queryString !== undefined){
+            let fieldsArr = queryString.split(',');
             let fields = { _id: 0, year:1}; 
 
-            queryParams.forEach(element => {
+            fieldsArr.forEach(element => {
                 fields[element] = 1;
             });
 
@@ -33,6 +33,59 @@ export const getYears = async (req, res) => {
         res.status(200).json(yearList);
     } catch (error) {
         res.status(404).json({message: error.message});
+    }
+}
+
+export const getSpecificYear = async (req, res) => {
+    try {
+        var year = req.params.year; 
+        var queryString = req.query.fields;
+
+        if(queryString !== undefined){
+            let fieldsArr = queryString.split(',');
+            let fields = { _id: 0, year:1}; 
+
+            fieldsArr.forEach(element => {
+                fields[element] = 1;
+            });
+
+            const arrestsYearData = await Arrests.find({year: year}, fields);
+    
+            res.status(200).json(arrestsYearData);
+        }else{
+            const arrestsYearData = await Arrests.find({year: year}, {_id: 0});
+
+            res.status(200).json(arrestsYearData);
+        }
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getYearRange = async (req, res) => {
+    try {
+        var yearStart = req.params.yearstart; 
+        var yearEnd = req.params.yearend;
+        var queryString = req.query.fields;
+
+        if(queryString !== undefined){
+            let fieldsArr = queryString.split(',');
+            let fields = { _id: 0, year:1}; 
+
+            fieldsArr.forEach(element => {
+                fields[element] = 1;
+            });
+
+            const arrestsYearsData = await Arrests.find({ year: { $gte: yearStart, $lte: yearEnd }}, fields);
+    
+            res.status(200).json(arrestsYearsData);
+        }else{
+            const arrestsYearsData = await Arrests.find({ year: { $gte: yearStart, $lte: yearEnd }}, {_id: 0});
+
+            res.status(200).json(arrestsYearsData);
+        }
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
--- api/routes/arrestsNational.js
+++ api/routes/arrestsNational.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
-import {getData, getArrestOffense, getYears } from '../controllers/arrestsNational.js';
+import {getData, getArrestOffense, getYears, getSpecificYear, getYearRange } from '../controllers/arrestsNational.js';
 
 const router = express.Router();
 
 router.get('/', getData);
 router.get('/years', getYears);
-//router.get('/years/:year', getSpecificYear);
-//router.get('/years/:yearstart/:yearend', getYearRange);
+router.get('/years/:year', getSpecificYear);
+router.get('/years/:yearstart/:yearend', getYearRange);
 router.get('/offenses', getArrestOffense);
 
 
