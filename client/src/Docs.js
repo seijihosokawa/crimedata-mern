ecc060eb6470dd6c531132a515ffc5f7f35a29c1
seijihosokawa <seijihosokawa@gmail.com>
2021-08-04 12:52:37 -0400

    dropdown added

--- client/src/Docs.js
+++ client/src/Docs.js
@@ -14,7 +14,7 @@ const Docs = () => {
         <div id="page" className="flex flex-no-wrap bg-gray-800">
             <div id="navbar" className="hidden md:block fixed h-screen">
                 <Navbar></Navbar>
-                <div className="text-white">
+                <div className="text-white transition duration-500 hover:text-gray-500">
                     <MainLink to="/">
                         <svg xmlns="http://www.w3.org/2000/svg" className="fixed right-12 top-4 h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
