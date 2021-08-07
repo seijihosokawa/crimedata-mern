9a2084b1ae14796c22335dedb0445da0bfe1a292
seijihosokawa <seijihosokawa@gmail.com>
2021-08-07 12:26:14 -0400

    added table

--- client/src/DataTable.js
+++ client/src/DataTable.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 const DataTable = () => {
     const [summaryData, setSummaryData] = useState([]);
+    const summaryCols = ["year", "population", "violent_crime", "homicide", "rape", "robbery", "aggravated_assault", "property_crime", "burglary", "larceny", "motor_vehicle_theft"]
 
     useEffect(() => {
         fetch("https://crimedata-mern.herokuapp.com/v1/summary/years-total/")
@@ -12,8 +13,28 @@ const DataTable = () => {
           });
       }, []);
     return (
-        <div>
+        <div className="text-sm overflow-y-auto">
+          <tr>
             {
+              summaryCols.map((item) => 
+              <th>{item}</th>)
+            }
+          </tr>
+            { summaryData.map(( {year, population, violent_crime, homicide, rape, robbery, aggravated_assault, property_crime, burglary, larceny, motor_vehicle_theft}) => (
+              <tr>
+                <td>{year}</td> 
+                <td>{population}</td>
+                <td>{violent_crime}</td>
+                <td>{homicide}</td>
+                <td>{rape}</td>
+                <td>{robbery}</td>
+                <td>{aggravated_assault}</td>
+                <td>{property_crime}</td>
+                <td>{burglary}</td>
+                <td>{larceny}</td>
+                <td>{motor_vehicle_theft}</td>
+              </tr>
+            ))
             }
         </div>
     )
--- client/src/Main.js
+++ client/src/Main.js
@@ -6,10 +6,9 @@ import DataTable from './DataTable';
 const Main = () => {
     const [currDataset, setCurrDataset] = useState('years-total')
     return (
-        <div id="page" className="bg-gray-800 h-screen">
+        <div id="page" className="bg-gray-800 h-full">
             <MainNavbar onChange={(value) => setCurrDataset(value)}/>
-            <div>
-                <p>{ currDataset }</p>
+            <div className="flex justify-center mx-auto w-5/6">
                 <DataTable/>
             </div>            
         </div>
